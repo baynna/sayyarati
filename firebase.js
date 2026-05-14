120145e4511a226396a8a56efed86fde391d6a99
baynna <hayderkareem654@gmail.com>
2026-05-14 23:05:42 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+getFirestore
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+getAuth
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyAF7HH6y4jx4DWeIR97nui09SQ46eHc6Iw",
+authDomain: "sayyarati-cars.firebaseapp.com",
+projectId: "sayyarati-cars",
+storageBucket: "sayyarati-cars.firebasestorage.app",
+messagingSenderId: "1023141775148",
+appId: "1:1023141775148:web:76ac773a46f1f4daaf4e89",
+measurementId: "G-ZKZZK90BEY"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+console.log("Firebase base file loaded");

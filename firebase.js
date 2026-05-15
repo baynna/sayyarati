2d34d61fb2959c53d81df659c32f2498748d89fe
baynna <hayderkareem654@gmail.com>
2026-05-15 14:39:40 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -15,19 +15,37 @@ getAuth
 }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+import {
+getStorage
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
 const firebaseConfig = {
+
 apiKey: "AIzaSyAF7HH6y4jx4DWeIR97nui09SQ46eHc6Iw",
+
 authDomain: "sayyarati-cars.firebaseapp.com",
+
 projectId: "sayyarati-cars",
+
 storageBucket: "sayyarati-cars.firebasestorage.app",
+
 messagingSenderId: "1023141775148",
+
 appId: "1:1023141775148:web:76ac773a46f1f4daaf4e89",
+
 measurementId: "G-ZKZZK90BEY"
+
 };
 
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = getApps().length
+? getApp()
+: initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
 
 console.log("Firebase base file loaded safely");

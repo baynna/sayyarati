ec13f7c9a4282a894ce9c2ee6b51b20df4642773
baynna <hayderkareem654@gmail.com>
2026-05-15 16:34:41 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -24,7 +24,7 @@ const firebaseConfig = {
 apiKey: "AIzaSyAF7HH6y4jx4DWeIR97nui09SQ46eHc6Iw",
 authDomain: "sayyarati-cars.firebaseapp.com",
 projectId: "sayyarati-cars",
-storageBucket: "sayyarati-cars.firebasestorage.app",
+storageBucket: "sayyarati-cars.appspot.com",
 messagingSenderId: "1023141775148",
 appId: "1:1023141775148:web:76ac773a46f1f4daaf4e89",
 measurementId: "G-ZKZZK90BEY"

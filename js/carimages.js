a50dc917738733c933bfe4c25793a3c25ac32ca0
baynna <hayderkareem654@gmail.com>
2026-05-15 15:17:00 +0300

    Update carimages.js

--- js/carimages.js
+++ js/carimages.js
@@ -1,25 +1,49 @@
-import { auth, db } from "./firebase.js";
+import { storage } from "./firebase.js";
 
 import {
-onAuthStateChanged
+ref,
+uploadBytes,
+getDownloadURL
 }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 console.log("carimages.js loaded successfully");
 
-// ملف صور السيارات
-
 const imageInput = document.getElementById("carImage");
+
+let uploadedImageUrl = "";
 
 if(imageInput){
 
-imageInput.addEventListener("change", function(){
+imageInput.addEventListener("change", async function(){
 
 const file = imageInput.files[0];
 
-if(file){
+if(!file){
+return;
+}
+
+try{
 
 console.log("تم اختيار صورة:", file.name);
+
+const imageRef = ref(storage, "car-images/" + Date.now() + "-" + file.name);
+
+await uploadBytes(imageRef, file);
+
+uploadedImageUrl = await getDownloadURL(imageRef);
+
+window.carImageUrl = uploadedImageUrl;
+
+console.log("تم رفع الصورة بنجاح:", uploadedImageUrl);
+
+alert("تم رفع الصورة بنجاح");
+
+}catch(error){
+
+console.log(error);
+
+alert("حدث خطأ أثناء رفع الصورة");
 
 }
 
